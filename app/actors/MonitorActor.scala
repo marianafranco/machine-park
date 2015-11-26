26ba4542e25e56aef9b70179e3ef94483f1e6d6d
Mariana Ramos Franco <ramos.franco@gmail.com>
2015-11-26 00:07:38 -0200

    GET of each machine

--- app/actors/MonitorActor.scala
+++ app/actors/MonitorActor.scala
@@ -2,7 +2,6 @@ package actors
 
 import akka.actor.{Actor, Cancellable}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.JsArray
 import play.api.libs.ws._
 import play.api.Play.current
 import play.Logger
@@ -15,10 +14,11 @@ import scala.util.{Failure, Success}
  */
 class MonitorActor extends Actor {
 
-  private val CHECK_MACHINES = "CHECK_MACHINES"
+  private val MONITOR = "MONITOR"
 
   private val MACHINE_PARK_API = "http://machinepark.actyx.io/api/v1"
   private val MACHINES_URL = MACHINE_PARK_API + "/machines"
+  private val MACHINE_URL = MACHINE_PARK_API + "/machine/"
 
   private var scheduler: Cancellable = _
 
@@ -27,7 +27,7 @@ class MonitorActor extends Actor {
       initialDelay = 0 seconds,
       interval = 5 seconds,
       receiver = self,
-      message = CHECK_MACHINES
+      message = MONITOR
     )
   }
 
@@ -35,23 +35,56 @@ class MonitorActor extends Actor {
     scheduler.cancel()
   }
 
+  def getMachineInfo(url: String): Future[String] = {
+    val id = url.substring(url.lastIndexOf("/") + 1)
+    Logger.debug("Getting machine with id " + id)
+
+    val holder: WSRequestHolder = WS.url(MACHINE_URL + id)
+    val complexHolder: WSRequestHolder =
+      holder.withHeaders("Accept" -> "application/json")
+
+    val futureResponse: Future[String] = complexHolder.get().map {
+      response =>
+        (response.json \ "name").as[String]
+    }
+
+    return futureResponse
+  }
+
+  def getMachines(): Future[List[String]] = {
+    Logger.debug("Getting all machines...")
+
+    val holder: WSRequestHolder = WS.url(MACHINES_URL)
+    val complexHolder: WSRequestHolder =
+      holder.withHeaders("Accept" -> "application/json")
+
+    val futureResponse: Future[List[String]] = complexHolder.get().map {
+      response =>
+        (response.json).as[List[String]]
+    }
+
+    return futureResponse
+  }
+
+
   def receive = {
-    case CHECK_MACHINES => {
-      Logger.debug("Monitoring all machines...")
-
-      val holder: WSRequestHolder = WS.url(MACHINES_URL)
-      val complexHolder: WSRequestHolder =
-        holder.withHeaders("Accept" -> "application/json")
-
-      val futureResponse: Future[List[String]] = complexHolder.get().map {
-        response =>
-          (response.json).as[List[String]]
-      }
+    case MONITOR => {
+      val futureResponse: Future[List[String]] = getMachines()
 
       futureResponse onComplete {
-        case Success(machines) => for (machine <- machines) Logger.debug(machine)
-        case Failure(t) => println("An error has occured: " + t.getMessage)
+        case Success(machines) => for (machine <- machines) {
+//          Logger.debug(machine)
+          val mFutureResponse = getMachineInfo(machine)
+
+          mFutureResponse onComplete {
+            case Success(name) => Logger.debug(name)
+            case Failure(t) => Logger.error("An error has occurred: " + t.getMessage)
+          }
+
+        }
+        case Failure(t) => Logger.error("An error has occurred: " + t.getMessage)
       }
     }
   }
+
 }
